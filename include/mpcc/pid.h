f77b15c1ac735bdae1a016e04cf86ef7536fabff
Prachit Amin <prachitamin12@gmail.com>
2025-01-23 19:16:35 -0500

    Switched from chrono library to ros2 timer for dt in PID control

--- include/mpcc/pid.h
+++ include/mpcc/pid.h
@@ -5,41 +5,41 @@
 
 #include <chrono>
 
-class PID {
+class PID
+{
 public:
-    PID() : prevTime_(std::chrono::high_resolution_clock::now()) {};
+    PID(double millis) : dt(1 / millis) {};
     void calculateError(double setpoint, double processVar);
     double P(double kP);
     double I(double kI);
     double D(double kD);
+
 private:
     double error;
     double prevError;
     double dt = 0;
     double integral = 0;
     double derivative = 0;
-    std::chrono::time_point<std::chrono::high_resolution_clock> prevTime_;
 };
 
-void PID::calculateError(double setpoint, double processVar) {
+void PID::calculateError(double setpoint, double processVar)
+{
     error = -(setpoint - processVar);
 }
 
-double PID::P(double kP) {
+double PID::P(double kP)
+{
     return kP * error;
 }
 
-double PID::I(double kI) {
-    auto currentTime_ = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> timeDiff_ = currentTime_ - prevTime_;
-    dt = timeDiff_.count();
-
+double PID::I(double kI)
+{
     integral += error * dt;
-    prevTime_ = currentTime_;
     return kI * integral;
 }
 
-double PID::D(double kD) {
-    derivative = (dt > 0) ? (error - prevError) / 2 : 0;
+double PID::D(double kD)
+{
+    derivative = (error - prevError) / 2;
     return kD * derivative;
 }
